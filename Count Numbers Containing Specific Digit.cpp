821ab1bf65687bf93540fcf7e8591dc9858ef608
varsha7104 <130915408+varsha7104@users.noreply.github.com>
2025-07-20 08:38:10 +0530

    Update Count Numbers Containing Specific Digit.cpp

--- Count Numbers Containing Specific Digit.cpp
+++ Count Numbers Containing Specific Digit.cpp
@@ -4,7 +4,7 @@ class Solution {
         // code here
          int tot=9*pow(10,n-1);
         int z=count(arr.begin(), arr.end(), 0);
-        int N=arr.size();
+        int N=arr.size(); 
         return tot-(9-N+z)*pow(10-N,n-1);
     }
 };
