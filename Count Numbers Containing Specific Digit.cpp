0ff9d4afcae57708042a07e55a778497df1f336a
varsha7104 <130915408+varsha7104@users.noreply.github.com>
2025-07-20 08:35:10 +0530

    Create Count Numbers Containing Specific Digit.cpp

---
+++ Count Numbers Containing Specific Digit.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int countValid(int n, vector<int>& arr) {
+        // code here
+         int tot=9*pow(10,n-1);
+        int z=count(arr.begin(), arr.end(), 0);
+        int N=arr.size();
+        return tot-(9-N+z)*pow(10-N,n-1);
+    }
+};
